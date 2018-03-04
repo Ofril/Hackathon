2fd84657afcd218542b408de9259dbfff2de51c4
Ofri <olahad24@gmail.com>
2018-03-05 01:56:50 +0200

    Routes

--- project/public/javascripts/index.js
+++ project/public/javascripts/index.js
@@ -1,6 +1,31 @@
 var myApp = angular.module('myApp', ['ngRoute']);
 
+myApp.config([function ($routeProvider , $locationProvider)] {
+    $routeProvider
+        .when("/", {
+            templateUrl: "../views/main.html",
+            controller: "myCtrl"
+        })
+        .when("/advancedSearch", {
+            templateUrl: "../views/Search.html",
+            controller: 'searchController'
+        })
+        .when("/newAccount", {
+            templateUrl: "../views/newAcount.html",
+            controller: "newAcountController"
+        })
+        .when("/myAccount", {
+            templateUrl: "../views/myAcount.html"
+        })
+        .otherwise({
+            redirectTo: '/myAccount'
+        });
+              
+    $locationProvider.html5Mode(true);
+});
+
 myApp.controller('myCtrl', function($scope) {
+    
     $scope.firstName= "John";
     $scope.lastName= "Doe";
 }); 
