e7d0a65b49854efc2bcd7147df4bd0543733a754
barbabrabr <bar180999@gmail.com>
2018-03-05 01:00:12 +0200

    routes - my account

--- project/public/javascripts/index.js
+++ project/public/javascripts/index.js
@@ -1,6 +1,16 @@
-var myApp = angular.module('myApp', []);
+var myApp = angular.module('myApp', ["ngRoute"]);
 
-myApp.controller('myCtrl', function($scope) {
-    $scope.firstName= "John";
-    $scope.lastName= "Doe";
-}); 
+myApp.config(function($routeProvider, $locationProvider) {
+    $locationProvider.hashPrefix('');
+    $routeProvider
+    .when("/photo", {
+        templateUrl : "Phothos.html"
+    })
+    .when("/shtap", {
+        templateUrl : "Shatap.html"
+    });
+});
+
+myApp.controller('photoCtrl', function($scope) {
+    $scope.photos = [{"src":"1.png"},{"src":"2.png"},{"src":"3.png"},{"src":"4.png"},{"src":"5.png"}];
+});
